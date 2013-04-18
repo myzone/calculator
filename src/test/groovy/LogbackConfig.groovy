b1dd5f02ef1fa52f4dfd723c1e51596345726749
Vyacheslav Goldenshteyn <vyacheslav.goldenshteyn@gmail.com>
2013-04-19 02:16:41 +0300

    Logback config has been fixed.

--- src/test/groovy/LogbackConfig.groovy
+++ src/test/groovy/LogbackConfig.groovy
@@ -1,10 +1,23 @@
 import com.mongodb.ServerAddress
 import com.myzone.utils.logging.LogbackMongoAppender
 
-appender("mongoAppender", LogbackMongoAppender) {
-    hosts = [new ServerAddress("127.0.0.1")]
-    databaseName = "testsLogs"
-    collectionName = "log_from__${timestamp("dd_MM_yyyy__HH_mm_ss")}"
+class GroovyAppender extends LogbackMongoAppender {
+
+    @Override
+    public List<ServerAddress> getHosts() {
+        return [new ServerAddress("127.0.0.1")]
+    }
+
+    @Override
+    public String getDatabaseName() {
+        return "testsLogs";
+    }
+
+    @Override
+    public String getCollectionName() {
+        return "log_from__${new Date().format("dd_MM_yyyy__HH_mm_ss")}"
+    }
 }
 
+appender("mongoAppender", GroovyAppender)
 root(ALL, ["mongoAppender"])
